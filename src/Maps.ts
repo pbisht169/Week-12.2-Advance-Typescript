9114b206cfaa57d37683290a65b2bf7f7f7531f8
pbisht17 <pawanbisht94.pb@gmail.com>
2024-05-10 01:27:38 +0530

    TS new concept

--- src/Maps.ts
+++ src/Maps.ts
@@ -7,3 +7,16 @@ const user = users.get('titu');
 console.log(users.get('titu'))
 console.log(users.get('jitu'))
 console.log(users)
+
+
+type Plane = {
+    name: string;
+    flightNo: number;
+}
+
+const flights = new Map<string, Plane>();
+
+flights.set('boeing117', { name: '3442sdw', flightNo: 233111 })
+flights.set('boeing119', { name: '34411rt', flightNo: 223322 })
+
+console.log(flights.get('boeing117'))
